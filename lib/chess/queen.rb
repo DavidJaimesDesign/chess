ddd843e5ab1bd79828cb9e0179d503c947925101
DavidJaimeDesign <davidjaimesdesign@gmail.com>
2016-08-11 22:17:51 -0700

    Started tdd for queen valid_move and jump_piece_recur

--- lib/chess/queen.rb
+++ lib/chess/queen.rb
@@ -7,5 +7,12 @@ module Chess
 				"\u265B".encode('utf-8')
 			end 
 		end
+
+		def valid_move?(coord_i, coord_f, board)
+			return "ERROR coordinates are nil" if coord_i.nil? || coord_f.nil?
+			if team_kill?(coord_f, board)
+				false
+			end
+		end
 	end
 end
--- spec/queen_spec.rb
+++ spec/queen_spec.rb
@@ -13,5 +13,56 @@ module Chess
 				expect(queen.display).to eq("\u265B".encode('utf-8'))
 			end
 		end
+
+		context "#piece_jump_recur" do
+		end
+
+		context "#valid_move" do 
+			before(:each) do
+			@queen_white  = Queen.new("white")
+			@queen_white2 = Queen.new("white")
+			@board        = Board.new
+			end
+
+			it "returns false if the move is a team kill" do
+				@board.board[0][0] = @queen_white
+				@board.board[0][1] = @queen_white2
+
+				ci = [0, 0]
+				cf = [0, 1]
+
+				expect(@queen_white.valid_move?(ci, cf, @board)).to be false 
+			end
+
+			it "checks false if the move is non linear(vartical, horizontal, diagonal(dy/dx = 1 || -1))" do
+			end
+
+			it "returns true for vertical positive movement" do
+			end
+
+			it "returns true for vertical negative movement" do
+			end
+
+			it "returns true for horizontal positive movement" do
+			end
+
+			it "returns true for horizontal negative movement" do
+			end
+
+			it "returns true for right up diagonal" do
+			end
+
+			it "returns true for right down diagonal" do
+			end
+
+			it "returns true for left up diagonal" do
+			end
+
+			it "returns true for left down diagonal" do
+			end
+
+			it "returns false for jumping over pieces" do
+			end
+		end
 	end
 end
