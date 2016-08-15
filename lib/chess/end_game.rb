a471366dc3f532b7819b720dc6eb488b2b83d1fc
David Jaimes <davidjaimesdesign@gmail.com>
2016-08-15 16:13:29 -0700

    TDD for #check and associated methods proceeds

--- lib/chess/end_game.rb
+++ lib/chess/end_game.rb
@@ -1,4 +1,25 @@
 module Chess
 	class End_Game
+		attr_accessor :king, :board
+		def initialize(king, board)
+			@king = king
+			@board = board
+		end
+
+		def check?
+			if self.kight_check?
+				true
+			else
+				false
+			end
+		end
+
+		def kight_check?
+			true
+		end
+
+		def horiz_check?
+			true
+		end
 	end
 end
--- spec/end_game_spec.rb
+++ spec/end_game_spec.rb
@@ -4,13 +4,39 @@ module Chess
 	describe End_Game do
 		#In one sentence it tells you if the game is over or close by way of check, check_mate, or stalemate
 		context "#initialize" do
-			it "takes a king piece, and a board" do
+			it "initializes with a king piece and a board piece" do
+				board = Board.new
+				king = King.new("white")
+
+				end_game = End_Game.new(king, board)
+
+				expect(end_game.king).to eq(king)
+				expect(end_game.board).to eq(board)
 			end
 		end
 
 		context "#check?" do
 			#returns true for any situation where the king is in check
+			before(:each) do
+				@board        = Board.new
+				@king   	  = King.new("white")
+				@black_knight = Knight.new("black")
+				@black_rook   = Rook.new("black")
+				@black_bishop = Bishop.new("black")
+				@black_queen  = Queen.new("black")
+				@black_pawn	  = Pawn.new("black")
+
+				@board.board[3][3] = @king
+				@board.board[4][1] = @black_knight
+				@board.board[6][3] = @black_rook
+				@board.board[6][6] = @black_bishop
+				@board.board[0][3] = @black_queen
+				@board.board[4][2] = @black_pawn
+
+				@end_game = End_Game.new(@king, @board)
+			end
 			it "returns true if the king is threatend by a knight" do
+				expect(@end_game.check?).to be true
 			end
 
 			it "returns true if the kinng is threatend by a rook in vert or horizontal" do
@@ -27,6 +53,9 @@ module Chess
 
 		end
 
+		context"#knight_check?" do
+		end
+		
 		context "#check_mate" do
 			#returns true for any situation where the king is in check_mate
 		end
